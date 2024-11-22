374086cd07dad8a60e82e8c0c4a8efab8fc28979
Lora Zubic <lamia.lora@gmail.com>
2024-11-22 09:31:42 +0100

    scan and refocus enabled

--- AutoScaningApp/app/src/main/java/com/lmx/autoscaningapp/DataWedgeReciever.kt
+++ AutoScaningApp/app/src/main/java/com/lmx/autoscaningapp/DataWedgeReciever.kt
@@ -6,22 +6,20 @@ import android.content.Intent
 import android.util.Log
 import android.widget.EditText
 
-class DataWedgeReciever(private val editText: EditText): BroadcastReceiver() {
+class DataWedgeReciever(private val editText: EditText) : BroadcastReceiver() {
     override fun onReceive(context: Context?, intent: Intent?) {
-        if (intent != null) {
-            if(intent.action == "com.lmx.autoscaningapp.SCAN") {
-                val barcodeData = intent.getStringExtra("com.symbol.datawedge.data_string")
-                Log.d("DataWedge", "Received barcode: $barcodeData")
+        if (intent != null && intent.action == "com.lmx.autoscaningapp.SCAN") {
+
+            val barcodeData = intent.getStringExtra("com.symbol.datawedge.data_string")
+            Log.d("DataWedge", "Received barcode: $barcodeData")
 
 
 
-                editText.post {
-                    editText.setText(barcodeData ?: "")
-                }
+            editText.post {
+                editText.setText(barcodeData ?: "")
             }
         } else {
             Log.w("DataWedge", "Received null Intent")
-            //testing
         }
     }
 }
--- AutoScaningApp/app/src/main/java/com/lmx/autoscaningapp/MainActivity.kt
+++ AutoScaningApp/app/src/main/java/com/lmx/autoscaningapp/MainActivity.kt
@@ -1,11 +1,15 @@
 package com.lmx.autoscaningapp
 
 
+import android.content.Context
 import android.content.IntentFilter
 import android.os.Build
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
 import android.util.Log
 import android.view.KeyEvent
+import android.view.inputmethod.InputMethodManager
 import android.widget.EditText
 import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AppCompatActivity
@@ -22,6 +26,31 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         editTextSifra1 = findViewById(R.id.editTextSifra1)
+        val editTextKod1 = findViewById<EditText>(R.id.editTextKod1)
+        val editTextKod2 = findViewById<EditText>(R.id.editTextKod2)
+        val editTextKod3 = findViewById<EditText>(R.id.editTextKod3)
+
+        editTextSifra1.addTextChangedListener(object : TextWatcher {
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {}
+
+            override fun afterTextChanged(s: Editable?) {
+                if (!s.isNullOrEmpty()) {
+                    editTextKod1.text.clear()
+                    editTextKod2.text.clear()
+                    editTextKod3.text.clear()
+
+                    editTextKod1.isEnabled = true
+                    editTextKod2.isEnabled = true
+                    editTextKod3.isEnabled = true
+
+                    editTextKod1.requestFocus()
+                    showKeyboard(editTextKod1)
+                }
+            }
+        })
+
 
         dataWedgeReceiver = DataWedgeReciever(editTextSifra1)
 
@@ -43,5 +72,11 @@ class MainActivity : AppCompatActivity() {
     override fun onDestroy() {
         super.onDestroy()
         unregisterReceiver(dataWedgeReceiver)
+    }
+
+    private fun showKeyboard(editText: EditText) {
+        editText.text.clear()
+        val imm = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+        imm.showSoftInput(editText, InputMethodManager.SHOW_IMPLICIT)
     }
 }
